bc3f093de956a44e773bd96bda2c537e8a427a1d
ArthurSuz <931446361@qq.com>
2022-08-31 16:04:58 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    assetsDir: './',
   },
   server: {
     host: '0.0.0.0',
